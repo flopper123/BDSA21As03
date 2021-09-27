5cbaaf14bad228a092791ce84559f1a829be6096
Ib Løwe Skovfoged Scherer <ibsc@itu.dk>
2021-09-27 11:18:59 +0200

    Implemented Flatten method as a one-liner

--- Assignment03/Extensions.cs
+++ Assignment03/Extensions.cs
@@ -1,33 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment03
 {
   public static class Extensions
   {
-
-
-    // Array.ForEach(xs, inner => foreach (var item in inner) { yield return item; });
-
-    // 
-
-
     public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items) {
-
-      // Error checking
-      if (items == null) yield break;
-      
-      // Iterate through out loop
-      foreach (IEnumerable<T> innerItems in items)
-      {
-        // Iterate through inner loop
-        foreach (var item in innerItems) {
-          // Return item
-          yield return item;
-        }
-      }
-
-      yield break;
+      return items.SelectMany(x => x.ToList());
     }
+
+    // public static IE
   }
 }
--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Assignment03
@@ -6,12 +7,8 @@ namespace Assignment03
   class Program
   {
     static void Main(string[] args)
-    {
-      int[] ys = new int[] {
-        2, 100, 4, 9, 74, 14 * 42, 7 * 42
-      };
-      int[] o = ys.Where(item => item % 7 == 0 && item > 42);
-      Console.WriteLine(o);
+        //done
     }
   }
 }
+
