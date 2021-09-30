43fc4681196c4f62d4d30c58f16b2f8a52669e23
Ib Løwe Skovfoged Scherer <74663809+Ib-Scherer@users.noreply.github.com>
2021-09-30 23:34:58 +0200

    Query Extensions (#5)
    fixed everything... It is done.

--- Assignment03.Tests/QueriesTests.cs
+++ Assignment03.Tests/QueriesTests.cs
@@ -4,22 +4,25 @@ using System.Collections.Generic;
 
 namespace Assignment03.Tests
 {
+
     public class QueriesTests
     {
+        static IEnumerable<Wizard> WizardDB = Wizard.Wizards.Value;
+
         [Fact]
         public void Returns_Character_Names_From_Rowling()
         {
             // Arrange
             IEnumerable<string> expected = new List<string>
-      {
-        "Harry Potter", "Snape", "Hagrid"
-      };
+              {
+                "Harry Potter", "Snape", "Hagrid"
+              };
 
             // Act
-            IEnumerable<string> LineActual = Queries.getWizardNamesFromRowlingLine();
-            IEnumerable<string> QueryActual = Queries.getWizardNamesFromRowlingQuery();
+            IEnumerable<string> QueryActual = Queries.getWizardNamesFromRowling(WizardDB);
+
             // Assert
-            Assert.Equal(expected, LineActual);
+            Assert.Equal(expected, WizardDB.getWizardNamesFromRowling());
             Assert.Equal(expected, QueryActual);
         }
 
@@ -30,10 +33,10 @@ namespace Assignment03.Tests
             int expected = 1977;
 
             // Act
-            var LineActual = Queries.getFirstSithLordLine();
-            var QueryActual = Queries.getFirstSithLordQuery();
+            var QueryActual = Queries.getFirstSithLord(WizardDB);
+
             // Assert
-            Assert.Equal(expected, LineActual);
+            Assert.Equal(expected, WizardDB.getFirstSithLord());
             Assert.Equal(expected, QueryActual);
         }
 
@@ -42,26 +45,38 @@ namespace Assignment03.Tests
         {
             // Arrange
             var expected = new List<(string, int?)>
-      {
-        ("Harry Potter", 1652),
-        ("Snape", 1888),
-        ("Hagrid", 2232)
-      };
+              {
+                ("Harry Potter", 1652),
+                ("Snape", 1888),
+                ("Hagrid", 2232)
+              };
 
             // Act
-            IEnumerable<(string, int?)> LineActual = Queries.getUniqueHPWizardsLine();
-            IEnumerable<(string, int?)> QueryActual = Queries.getUniqueHPWizardsQuery();
+            IEnumerable<(string, int?)> QueryActual = Queries.getUniqueHPWizards(WizardDB);
+
             // Assert
-            Assert.Equal(expected, LineActual);
+            Assert.Equal(expected, WizardDB.getUniqueHPWizards());
             Assert.Equal(expected, QueryActual);
 
         }
-
+        
         [Fact]
-        public void Should_Return_Correct_Answer()
+        public void return_List_Sorted_By_Author_then_Name()
         {
+            var expected = new List<string>
+            {
+              "Eggplant Wizard", "Baron Mordo", "Wizard of Oz", "Sauron", "Hagrid", "Harry Potter", "Snape", "Darth Maul", "Darth Vadar", "Ice King"
+            };
 
+            foreach (var item in expected)
+            {
+              Console.WriteLine(item);
+            }
 
+            IEnumerable<string> QueryActual = Queries.getWizardNamesByCreatorInReverseThenWizard(WizardDB);
+
+            Assert.NotStrictEqual(expected, QueryActual);
+            Assert.NotStrictEqual(expected, WizardDB.getWizardNamesByCreatorInReverseThenWizard());
         }
     }
 }
