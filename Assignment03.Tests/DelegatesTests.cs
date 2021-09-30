7b14bc30194ae12ff638c9063b79f7bbae87ea22
Thomas <thomas.schauser@gmail.com>
2021-09-30 22:01:01 +0200

    Delegates (#2)
    Delegates

---
+++ Assignment03.Tests/DelegatesTests.cs
@@ -0,0 +1,115 @@
+using System;
+using Xunit;
+using System.Linq;
+
+namespace Assignment03.Tests
+{
+  public class DelegatesTests
+  {
+
+    [Theory]
+    [InlineData(new object[] { "abc", "cba" })]
+    [InlineData(new object[] { "Hello World!", "!dlroW olleH" })]
+    [InlineData(new object[] { "Skrrrt", "trrrkS" })]
+    public void ReturnsReversedString(string input, string exp)
+    {
+      // Arranage
+      string expected = exp;
+
+      Func<string, string> stringOperation;
+      stringOperation = str => str.ToArray().Reverse().Aggregate("", (acc, c) => acc + c);
+
+      // Act
+      string actual = stringOperation(input);
+
+      // Assert
+      Assert.Equal(expected, actual);
+
+    }
+
+    [Theory]
+    [InlineData(new object[] { 5, 5, 25 })]
+    [InlineData(new object[] { 1.5, 4, 6 })]
+    [InlineData(new object[] { -1, 100, -100 })]
+    public void ReturnsCorrectProduct(decimal x, decimal y, decimal exp)
+    {
+      // Arranage
+      decimal expected = exp;
+
+      Func<decimal, decimal, decimal> decimalOperation;
+      decimalOperation = (a, b) => a * b;
+
+      // Act
+      decimal actual = decimalOperation(x, y);
+
+      // Assert
+      Assert.Equal(expected, actual);
+
+    }
+
+
+
+    [Theory]
+    [InlineData(new object[] { " 0042", 0042 })]
+    [InlineData(new object[] { "1142", 1142 })]
+    [InlineData(new object[] { "11 42", 1142 })]
+    [InlineData(new object[] { "  000 A 001", 1 })]
+    [InlineData(new object[] { "23 b 456", 23456 })]
+    public void StringShouldMatchNumber(string input, int exp)
+    {
+      // Arranage
+      int expected = exp;
+
+      Func<string, int, bool> stringIntOperation;
+      stringIntOperation =
+        (str, num) => num == str
+          .ToArray()
+          .Aggregate(0,
+            (acc, c) => {
+              int n = c - '0';
+              if (n > 9 || n < 0) return acc;
+              return 10*acc + n;
+            }
+          );
+
+      // Act
+      bool compared = stringIntOperation(input, exp);
+
+      // Assert
+      Assert.True(compared);
+    }
+
+    [Theory]
+    [InlineData(new object[] { "AABB", 27 })]
+    [InlineData(new object[] { "SKRT 23", 35 })]
+    [InlineData(new object[] { "oui", 33 })]
+    [InlineData(new object[] { "  001 A 001", 1 })]
+    [InlineData(new object[] { "23 b 4056", 23456 })]
+    public void StringNotMatchNumber(string input, int exp)
+    {
+      // Arranage
+      int expected = exp;
+
+      Func<string, int, bool> stringIntOperation;
+      stringIntOperation =
+        (str, num) => num == str
+          .ToArray()
+          .Aggregate(0,
+            (acc, c) =>
+            {
+              int n = c - '0';
+              if (n > 9 || n < 0) return acc;
+              return 10 * acc + n;
+            }
+          );
+
+      // Act
+      bool compared = stringIntOperation(input, exp);
+
+      // Assert
+      Assert.False(compared);
+    }
+
+
+  }
+}
